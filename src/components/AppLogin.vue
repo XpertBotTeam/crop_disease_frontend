ca61d9016cabdfe92d08cb51314a992313b4345d
Elie-Alhaddad <=>
2023-09-03 23:29:59 +0300

    API for login and registration

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,30 +1,43 @@
 <script>
+import axios from 'axios';
 export default {
   name: "AppLogin",
   data() {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
+      attemptsMsg: '',
     };
   },
   methods: {
-    signInWithGoogle() {
-      // Implement Google Sign-In logic here
-    },
-    login() {
-      if (!this.validateForm()) {
-        return; // Validation failed, do not proceed
+    
+    async login() {
+     try{
+      let response = await axios.post(
+        "https://cdim.xpertbotacademy.online/api/login",
+        {
+          email:this.email,
+          password: this.password,
+          
+        }
+      );
+      console.log(response);
+      if(response.status == 200 && response.data.success === true){
+        localStorage.setItem("user-info",JSON.stringify(response.success));
+        this.$router.push("/");
+      
       }
+      else {
+  
+  alert( "Login failed. Please check your credentials.");
+  this.$router.push("/login");
+}
+      console.log(response.success);
+     }catch(error){
+      console.log(error);
+     }
+    },
 
-      // Continue with login logic
-    },
-    validateForm() {
-      if (!this.email || !this.password) {
-        alert('Please fill in all fields.');
-        return false;
-      }
-      return true;
-    },
   },
 };
 </script>
@@ -36,24 +49,26 @@ export default {
       <div class="col-md-6 offset-md-3">
         <h2 class="text-center text-dark mt-5">Login Page</h2>
         <div class="card my-5">
-          <form @submit.prevent="login" class="card-body cardbody-color p-lg-5">
+          <form @submit.prevent="login" method="post" class="card-body cardbody-color p-lg-5">
+            <div class="errors">{{ attemptsMsg }}</div>
             <div class="mb-3">
               <input
                 type="email"
+                v-model="email"
                 class="form-control"
                 id="email"
                 placeholder="email@crop_disease"
-                v-model="email"
-                required
+                   required
               />
             </div>
             <div class="mb-3">
               <input
                 type="password"
+                v-model="password"
                 class="form-control"
                 id="password"
                 placeholder="Password"
-                v-model="password"
+                
                 required
               />
             </div>
@@ -64,14 +79,7 @@ export default {
               </button>
             </div>
 
-            <div class="text-center">
-              <button
-                @click="signInWithGoogle"
-                class="btn btn-danger px-5 mb-2 w-100"
-              >
-                Sign in with Google
-              </button>
-            </div>
+         
             <div class="form-text text-center mb-5 text-dark">
               Not Registered?
               <router-link to="/register" class="text-dark fw-bold">
--- src/components/AppRegister.vue
+++ src/components/AppRegister.vue
@@ -1,48 +1,103 @@
 <script>
+import axios from 'axios';
+
 export default {
   name: "AppRegister",
+  data() {
+    return {
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      errorMessage: "", 
+    };
+  },
+  methods: {
+    async signup() {
+      
+      if (this.password !== this.confirmPassword) {
+        
+        this.errorMessage = "Passwords do not match.";
+        return;
+      }
+      this.errorMessage = "";
+      try {
+        let result = await axios.post("https://cdim.xpertbotacademy.online/api/register", {
+          name: this.username,
+          email: this.email,
+          password: this.password,
+        });
+
+        
+        if (result.status === 200 & result.data.status === true) {
+          alert("Registration successful. You can now log in.");
+          this.$router.push("/login"); 
+          
+        
+        } else {
+          
+          this.errorMessage = "Registration failed. Please check your information.";
+        }
+      } catch (error) {
+        
+        this.errorMessage = "An error occurred during registration.";
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
 
 <template>
-   <div class="container">
+  <div class="container">
     <div class="row">
       <div class="col-md-6 offset-md-3">
         <h2 class="text-center text-dark mt-5">Register Page</h2>
         <div class="card my-5">
-          <form class="card-body cardbody-color p-lg-5">
+          <form class="card-body cardbody-color p-lg-5" @submit.prevent="signup">
             <div class="mb-3">
               <input
-                type="text"
+                type="text" 
+                v-model="username"
                 class="form-control"
                 id="username"
                 placeholder="UserName"
+                required
               />
             </div>
             <div class="mb-3">
               <input
                 type="email"
+                v-model="email"
                 class="form-control"
                 id="email"
                 placeholder="email@crop_disease"
+                required
               />
             </div>
             <div class="mb-3">
               <input
                 type="password"
+                v-model="password"
                 class="form-control"
                 id="password"
                 placeholder="Password"
+                required
               />
             </div>
-
             <div class="mb-3">
               <input
-                type="Confirmpassword"
+                type="password"
+                v-model="confirmPassword"
                 class="form-control"
-                id="Confirmpassword"
+                id="confirmPassword"
                 placeholder="Confirm Password"
+                required
               />
+            </div>
+            
+            <div v-if="errorMessage" class="text-danger mb-3">
+              {{ errorMessage }}
             </div>
             <div class="text-center">
               <button type="submit" class="btn btn-primary px-5 mb-5 w-100">
@@ -50,10 +105,10 @@ export default {
               </button>
             </div>
             <div class="form-text text-center mb-5 text-dark">
-                Registered?
+              Registered?
               <router-link to="/login" class="text-dark fw-bold">
-                Login to your Account</router-link
-              >
+                Login to your Account
+              </router-link>
             </div>
           </form>
         </div>
@@ -61,4 +116,6 @@ export default {
     </div>
   </div>
 </template>
- 
+
+
+
